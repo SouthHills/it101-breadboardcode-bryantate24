342a4158d84947b1011a073911a6b76962ef76ad
bryantate24 <btate21@southhills.edu>
2025-04-15 14:42:55 -0400

    BT-BreadboardCode

--- 01_Blink/Blink.py
+++ 01_Blink/Blink.py
@@ -1,22 +1,28 @@
 from gpiozero import LED as LEDClass # Alias
 import time
 
-LED = LEDClass(17)  # define led
+LED = LEDClass(17)# define led
+LED2 = LEDClass(18)
 
 def loop():
-    global LED
+    global LED, LED2
     while True:
-        LED.on() 
+        LED.on()
+        LED2.off() 
         print ("led turned on >>>") # print information on terminal
         time.sleep(1)
         LED.off()
+        LED2.on()
         print ("led turned off <<<")
-        time.sleep(1)
+        time.sleep(1)   
+        
+        
         
 def destroy():
-    global LED
+    global LED, LED2
     # Release resources
     LED.close()
+    LED2.close()
 
 if __name__ == "__main__":    # Program start point
     print("Program is starting ... \n")
