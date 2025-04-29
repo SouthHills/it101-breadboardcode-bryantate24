ae440f51c140803561f395b103694fad6fbd2fb4
bryantate24 <btate21@southhills.edu>
2025-04-29 14:49:34 -0400

    BT-Emergency

---
+++ 06.2_Alertor/06.3_Alterations/Emergency.py
@@ -0,0 +1,55 @@
+from gpiozero import TonalBuzzer, Button, PWMLED
+import time
+from signal import pause
+
+BUZZER = TonalBuzzer(17)
+BUTTON = Button(18)
+HIGH_TONE = 600 # The max is 880 but that hurts my ears
+LOW_TONE = 220
+LED = PWMLED(23)
+buzzer_active = False
+    
+def setup():
+    # Setup events for when the button is pressed and released
+    BUTTON.when_pressed = alertor
+    BUTTON.when_released = stop_alertor
+    
+def alertor():
+    global LED
+    print ('alertor turned on >>> ')
+    diff = HIGH_TONE - LOW_TONE
+    while True:
+        #Linear
+        for x in range(LOW_TONE, HIGH_TONE):
+            BUZZER.play(x)
+            LED.value = (x - LOW_TONE) / diff
+            time.sleep(0.002)
+            
+            if not BUTTON.is_pressed:
+                return
+                
+        for x in range(HIGH_TONE, LOW_TONE, -1):
+            BUZZER.play(x)
+            LED.value = (x - LOW_TONE) / diff
+            time.sleep(0.002)
+        
+            if not BUTTON.is_pressed:
+                return
+
+def stop_alertor():
+    BUZZER.stop()
+    LED.off()
+    print ('alertor turned off <<<')
+
+def destroy():
+    BUZZER.close()
+    BUTTON.close()
+    LED.close()
+
+if __name__ == '__main__':     # Program entrance
+    print ('Program is starting...')
+    setup()
+    try:
+        pause()
+    except KeyboardInterrupt:  # Press ctrl-c to end the program.
+        destroy()
