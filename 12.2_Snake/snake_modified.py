388f2f4a8de6315ef9478f82510b84c17904aee3
bryantate24 <btate21@southhills.edu>
2025-05-15 14:22:00 -0400

    BT_SnakeAlteration

--- 12.2_Snake/snake_modified.py
+++ 12.2_Snake/snake_modified.py
@@ -5,6 +5,8 @@ import time
 import random
 from collections import deque
 from input_structure import Input
+from joystick import Joystick
+
 
 pygame.init()
 
@@ -23,6 +25,8 @@ BLACK = (0, 0, 0)
 
 # Snake and food sizes
 SNAKE_SPEED = 8
+
+JOYSTICK = Joystick()
 
 # Input poll (ms)
 INPUT_POLL = 20
@@ -57,16 +61,16 @@ def check_input():
     global current_direction, JOYSTICK
     # TODO: Replace "False" with the correct method call to the joystick object you make
     # Can't double-back on your snake
-    if False and current_direction != Input.RIGHT:
+    if JOYSTICK.get_direction() == "Left" and current_direction != Input.RIGHT:
         input_buffer.append(Input.LEFT)
-    elif False and current_direction != Input.LEFT:
+    elif JOYSTICK.get_direction() == "Right" and current_direction != Input.LEFT:
         input_buffer.append(Input.RIGHT)
-    elif False and current_direction != Input.DOWN:
+    elif JOYSTICK.get_direction() == "Up" and current_direction != Input.DOWN:
         input_buffer.append(Input.UP)
-    elif False and current_direction != Input.UP:
+    elif JOYSTICK.get_direction() == "Down" and current_direction != Input.UP:
         input_buffer.append(Input.DOWN)
     
-    if False: # Pressing in on the joystick should pause the game
+    if JOYSTICK.get_button_pressed(): # Pressing in on the joystick should pause the game
         input_buffer.append(Input.PRESS)
 
 # Function to main loop
@@ -193,7 +197,7 @@ def game_loop():
                         game_over = True
                         
             # TODO: Replace "False" with a call to the appropriate method in your joystick instance
-            if False:
+            if JOYSTICK.get_button_pressed():
                 game_close = False
                 game_over = False
 
